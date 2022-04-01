8a5bc1e5f40f9f116b551a66e11afb2b07d73723
Armin Schäfer <arminnessie@gmail.com>
2022-04-01 17:38:54 +0200

    Finish Iverson bracket.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 struct Point2 {
-    x: f32,
-    y: f32,
+    x: usize,
+    y: usize,
 }
 
 struct PiecewiseDefinedFunction {
@@ -9,7 +9,7 @@ struct PiecewiseDefinedFunction {
 
 struct Function {
     start: Point2,
-    vertex: Option<Point2>,
+    stopover: Option<Point2>,
     end: Point2,
 }
 
@@ -18,25 +18,36 @@ enum Notation {
 }
 
 impl Function {
-    fn print(self, notation: &Notation) {
-        match notation {
-            Notation::IversonBracket => match self.vertex {
-                Some(_vertex) => {
-                    println!(
-                        "quadratic_function * [{} <= x <= {}] +",
-                        self.start.x, self.end.x
-                    );
-                }
-                None => {
-                    let func = ((self.end.y - self.start.y) / (self.end.x - self.start.x))
-                        .to_string()
-                        + " * (x - "
-                        + &self.start.x.to_string()
-                        + ") + "
-                        + &self.start.y.to_string();
-                    println!("{} * [{} <= x <= {}] +", func, self.start.x, self.end.x);
-                }
-            },
+    fn print(self, _notation: &Notation) {
+        match self.stopover {
+            Some(stopover) => {
+                let x1: f32 = self.start.x as f32;
+                let x2: f32 = stopover.x as f32;
+                let x3: f32 = self.end.x as f32;
+                let y1: f32 = self.start.y as f32;
+                let y2: f32 = stopover.y as f32;
+                let y3: f32 = self.end.y as f32;
+                let a: f32 = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+                    / ((x1 - x2) * (x1 - x3) * (x3 - x2));
+                let b: f32 = (x1 * x1 * (y2 - y3) + x2 * x2 * (y3 - y1) + x3 * x3 * (y1 - y2))
+                    / ((x1 - x2) * (x1 - x3) * (x2 - x3));
+                let c: f32 = (x1 * x1 * (x2 * y3 - x3 * y2)
+                    + x1 * (x3 * x3 * y2 - x2 * x2 * y3)
+                    + x2 * x3 * y1 * (x2 - x3))
+                    / ((x1 - x2) * (x1 - x3) * (x2 - x3));
+                println!(
+                    "{} * x^2 + {} * x + {} * [{} <= x <= {}] +",
+                    a, b, c, self.start.x, self.end.x
+                );
+            }
+            None => {
+                let func = ((self.end.y - self.start.y) / (self.end.x - self.start.x)).to_string()
+                    + " * (x - "
+                    + &self.start.x.to_string()
+                    + ") + "
+                    + &self.start.y.to_string();
+                println!("{} * [{} <= x <= {}] +", func, self.start.x, self.end.x);
+            }
         }
     }
 }
@@ -46,6 +57,7 @@ impl PiecewiseDefinedFunction {
         for function in self.function {
             function.print(notation);
         }
+        println!("0");
     }
 }
 
@@ -53,34 +65,34 @@ fn main() {
     let a_different_step_function = PiecewiseDefinedFunction {
         function: vec![
             Function {
-                start: Point2 { x: 0.0, y: 0.0 },
-                vertex: Some(Point2 { x: 5.0, y: 20.0 }),
-                end: Point2 { x: 10.0, y: 0.0 },
+                start: Point2 { x: 0, y: 0 },
+                stopover: Some(Point2 { x: 5, y: 5 }),
+                end: Point2 { x: 10, y: 0 },
             },
             Function {
-                start: Point2 { x: 10.0, y: 0.0 },
-                vertex: None,
-                end: Point2 { x: 20.0, y: 10.0 },
+                start: Point2 { x: 10, y: 0 },
+                stopover: None,
+                end: Point2 { x: 20, y: 10 },
             },
             Function {
-                start: Point2 { x: 20.0, y: 10.0 },
-                vertex: Some(Point2 { x: 25.0, y: 15.0 }),
-                end: Point2 { x: 30.0, y: 10.0 },
+                start: Point2 { x: 20, y: 10 },
+                stopover: Some(Point2 { x: 25, y: 15 }),
+                end: Point2 { x: 30, y: 10 },
             },
             Function {
-                start: Point2 { x: 30.0, y: 10.0 },
-                vertex: None,
-                end: Point2 { x: 40.0, y: 20.0 },
+                start: Point2 { x: 30, y: 10 },
+                stopover: None,
+                end: Point2 { x: 40, y: 20 },
             },
             Function {
-                start: Point2 { x: 40.0, y: 20.0 },
-                vertex: Some(Point2 { x: 45.0, y: 25.0 }),
-                end: Point2 { x: 50.0, y: 20.0 },
+                start: Point2 { x: 40, y: 20 },
+                stopover: Some(Point2 { x: 45, y: 25 }),
+                end: Point2 { x: 50, y: 20 },
             },
             Function {
-                start: Point2 { x: 50.0, y: 20.0 },
-                vertex: None,
-                end: Point2 { x: 60.0, y: 30.0 },
+                start: Point2 { x: 50, y: 20 },
+                stopover: None,
+                end: Point2 { x: 60, y: 30 },
             },
         ],
     };
